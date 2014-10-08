1069e2811ae6d03991eb7e42010a041776fa7957
Andrew <andrewjamest1993@gmail.com>
2014-10-08 12:41:37 -0500

    Added function 'call_execve_dup2()' for printing output to file

--- ysh.c
+++ ysh.c
@@ -153,6 +153,34 @@ void call_execve(char *cmd)
     }
 }
 
+
+//This new function will print the output of a command into the file "myfile.txt". Note that "myfile.txt" must exist beforehand. -Andrew
+void call_execve_dup2(char *cmd)
+{
+    int i;
+    printf("cmd is %s\n", cmd);
+    if(fork() == 0) {
+	int file = open("myfile.txt", O_TRUNC | O_WRONLY);
+	int old_stdout = dup(1);
+	dup2(file,1);
+	
+
+        //i = execve(cmd, my_argv, my_envp);
+	i = execvp(cmd, my_argv); //This fixed the 'echo' problem on my machine. execve(3) does not search for the command on the default PATH, but execvp does. I don't know if this will cause any problems down the line, as execvp(2) does not take the list of environment variables (my_envp) as an argument. -Andrew
+        dup2(old_stdout,1);
+
+
+	printf("errno is %d\n", errno);
+        if(i < 0) {
+            printf("%s: %s\n", cmd, "command not found"); //This is the error message being printed from 'echo'. The error spawns from the value of 'i', which is assigned by the function 'execve(cmd, my_argv, my_envp); -Andrew
+            exit(1);        
+        }
+    } else {
+        wait(NULL);
+    }
+}
+
+
 void free_argv()
 {
     int index;
